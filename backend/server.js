dcc83988c0a5c5c3744b9cbd5a63176858fd0151
agentfeux <40972980+agentfeux@users.noreply.github.com>
2022-05-29 03:43:15 -0400

    Heroku config

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
@@ -11,12 +12,22 @@ const PORT = process.env.PORT || 8000;
 // Connect to mongoDB database
 connectDB();
 
-app.get("/", (req, res) => {
-  res.status("200").json({ message: "Bruh" });
-});
-
 // Allowed Routes
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/tickets", require("./routes/ticketRoutes"));
+
+//Serve Frontend
+if (process.env.NODE_ENV === "production") {
+  //Set build folder as static
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(__dirname, "../", "frontend", "build", "index.html")
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.status("200").json({ message: "Welcome to the Support System API" });
+  });
+}
 app.use(errorHandler);
 app.listen(PORT, () => console.log(`Server starting on port ${PORT}`));
